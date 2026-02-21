bac5db6d589f867aad03d7f70f94b5c022e3b8d6
blackwalt608 <merteliacik@hotmail.com>
2026-02-21 21:50:44 +0300

    navFix

--- src/components/ui/layout/hero/nav/Nav.tsx
+++ src/components/ui/layout/hero/nav/Nav.tsx
@@ -3,17 +3,17 @@ import Logo from "./Logo";
 
 export default function Nav() {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 py-9 h-[10vh]">
-      <ul className="w-[90%] mx-auto flex justify-between items-center">
+    <nav className="fixed top-0 left-0 w-screen z-50 py-9 h-[10vh] ">
+      <ul className="w-[90%] mx-auto flex justify-between items-center ">
         <li className="max-md:scale-70 max-md:origin-left">
           <Logo />
         </li>
 
-        <li>
+        <li className="max-md:-translate-x-6">
           <Button
             text="Contact"
             href="https://x.com/mertelia"
-            className="max-md:[--btn-fs:1.1rem] max-md:[--btn-h:1.6rem] md:[--btn-fs:1.56rem] md:[--btn-h:1.75rem]"
+            className="max-md:[--btn-fs:1.1rem]   max-md:[--btn-h:1.6rem] md:[--btn-fs:1.56rem] md:[--btn-h:1.75rem]"
           />
         </li>
       </ul>
